8248db5171b8126977b0519934fc42f62143fe2d
Sougata-9932 <145041254+Sougata-9932@users.noreply.github.com>
2024-02-26 16:13:53 +0530

    Update Hours_Format.cpp

--- Hours_Format.cpp
+++ Hours_Format.cpp
@@ -6,11 +6,7 @@
 
 double Hours_Format::calc_duration(Time& ex_t, Time& en_t)
 {
-	std::cout << ex_t.get_hours() << " " << ex_t.get_mins() << " " << ex_t.get_secs() << std::endl;
-	std::cout << en_t.get_hours() << " " << en_t.get_mins() << " " << en_t.get_secs() << std::endl;
 	double res_1 = (ex_t.get_hours()) + (ex_t.get_mins() / (double)60) + (ex_t.get_secs() / 3600);
 	double res_2 = (en_t.get_hours()) + (en_t.get_mins() / (double)60) + (en_t.get_secs() / 3600);
-    std::cout << "HOURS_FORMAT\n";
-	std::cout << res_1 << " " << res_2 << std::endl;
 	return res_1 - res_2;
 }
